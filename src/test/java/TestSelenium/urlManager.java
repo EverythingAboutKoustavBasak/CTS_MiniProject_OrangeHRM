d02e9ff4a0318c56ec4431c9b4e43a59e544658b
Koustav Basak <koustav23042001@gmail.com>
2024-04-21 09:38:59 +0530

    first commit

---
+++ src/test/java/TestSelenium/urlManager.java
@@ -0,0 +1,27 @@
+package TestSelenium;
+
+import java.io.IOException;
+
+import utilityFiles.ExcelUtils;
+
+public class urlManager extends Implementation{
+	//1 Opening the url
+		public void openUrl() throws IOException{
+			
+			// getting the url from excel.
+			String file=System.getProperty("user.dir")+"//Test_Data//testexcel.xlsx"; //get the excel sheet path
+			String baseURL = ExcelUtils.getCellData(file, "Sheet1", 1, 0); //Read URL from testexcel.xlsx, Sheet1
+			
+			driver.get(baseURL);
+			
+			
+			// Validating if the URL is opened properly or not
+			if(driver.getCurrentUrl().equals(baseURL)) {
+				ExcelUtils.setCellData(file, "Sheet1", 1, 1, "Passed/Opened"); // Write result to testexcel.xlsx, Sheet1
+				ExcelUtils.fillGreenColor(file, "Sheet1", 1, 1);
+			}else {
+				ExcelUtils.setCellData(file, "Sheet1", 1, 1, "Failed/Unable to Open"); // Write result to testexcel.xlsx, Sheet1
+				ExcelUtils.fillRedColor(file, "Sheet1", 1, 1);
+			}
+		}
+}
